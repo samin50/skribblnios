27dd7782c964b53fc33a240d8bd7e39da5335e66
Shaanukstar123 <shaanukag0@gmail.com>
2022-02-24 18:16:07 +0000

    ui working

--- Game/client.py
+++ Game/client.py
@@ -1,6 +1,5 @@
 import pygame
 import player
-
 class Game():
     def __init__(self,username):
         self.username = username
@@ -14,29 +13,32 @@ class Game():
         self.brushes = []
         self.colours = []
         self.timer = 100
+        self.background = pygame.image.load("assets/sky_background.png")
 
     def redraw_window(self):
         print(self.colours)
-        #self.display.fill(self.colours["white"])
+        self.display.fill((255, 255, 255))
 
     def round_start(self, x,y):
         pygame.draw.circle()
         
 
+
 game = Game("shan")
 run = True
 pygame.display.set_caption("player")
-game.redraw_window() 
-background=pygame.image.load("assets/sky_background.png")
-#background=pygame.transform.scale(background,(game.width,game.height))
+ 
+game.background=pygame.transform.scale(game.background,(game.width,game.height))
 
 while run == True:
-    game.display.blit(background,(400,-300))
+    game.events = pygame.event.get()
+    game.clock.tick(60)
+    game.redraw_window()
+    game.display.blit(game.background,(400,-300))
+    pygame.display.update()
+
     for event in game.events:
-        
         if event.type == pygame.QUIT:
             run = False
             pygame.quit()
-
-    pygame.display.update()
-    game.clock.tick(60)
+    
