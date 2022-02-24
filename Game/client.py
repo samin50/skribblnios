abfb40d39c83dbac7bffc671806cd6af1cd38f17
Shaanukstar123 <shaanukag0@gmail.com>
2022-02-24 18:48:03 +0000

    plot pairs working

--- Game/client.py
+++ Game/client.py
@@ -1,5 +1,6 @@
 import pygame
 import player
+import random
 class Game():
     def __init__(self,username):
         self.username = username
@@ -14,31 +15,43 @@ class Game():
         self.colours = []
         self.timer = 100
         self.background = pygame.image.load("assets/sky_background.png")
+        self.brush_size = 20
 
     def redraw_window(self):
         print(self.colours)
         self.display.fill((255, 255, 255))
 
-    def round_start(self, x,y):
-        pygame.draw.circle()
-        
-
+    def round_start(self, x,y,size):
+        pygame.draw.circle(game.display,(0,0,0),(x,y),size)
 
 game = Game("shan")
 run = True
 pygame.display.set_caption("player")
- 
-game.background=pygame.transform.scale(game.background,(game.width,game.height))
 
+def return_x():
+    x = random.randint(0,game.width)
+    return x
+
+def return_y():
+    y = random.randint(0, game.height)
+    return y
+
+game.background=pygame.transform.scale(game.background,(game.width,game.height))
+game.redraw_window()
+game.display.blit(game.background,(0,0))
+y = 0
+x = 0
 while run == True:
     game.events = pygame.event.get()
-    game.clock.tick(60)
-    game.redraw_window()
-    game.display.blit(game.background,(400,-300))
+
+    game.clock.tick(144)
+    game.brush_size = 5 #random.randint(0,50)
+    y +=1
+    x +=1
+    game.round_start(x,y,game.brush_size)
     pygame.display.update()
 
     for event in game.events:
         if event.type == pygame.QUIT:
             run = False
             pygame.quit()
-    
