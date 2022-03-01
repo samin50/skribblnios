d9838191d193e39e0c574b3c7e9739608c274158
rana2302 <67081360+rana2302@users.noreply.github.com>
2022-03-01 19:46:51 +0000

    Add files via upload

--- Communicator.py
+++ Communicator.py
@@ -1,1 +1,28 @@
-print("Communicator started.")
+import socket
+import pickle
+
+class Client_side:
+   def __init__(user): 
+         user.client = socket.socket # type of connection determined by indraneel and ziyad ,# how the serevr string comes in which is also determined by indraneel and ziyad)
+         user.host = #determined by Zi and Ind in the server file.
+         user.port = 2323
+         user.p = user.connect()
+         print(user.connect()) # prints that we have connected 
+
+   def getPosition(user):
+        return user.position
+
+   def connection_with_user(user):
+        try:
+            user.client.connect(user.host,user.port)
+            return pickle.loads(user.client.recv(1024))
+        except:
+            pass 
+
+   def send_data(user, data):
+        try:
+            user.client.send(pickle.dumps(data))
+            return pickle.loads(user.client.recv())
+        except socket.error as error:
+            print(error)
+
---
+++ untitled folder/Server.py
@@ -0,0 +1,53 @@
+import socket
+from _thread import *
+import Player
+import pickle
+
+server = "5.151.46.65"
+port = 6942
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((server, port))
+except socket.error as e:
+    str(e)
+
+s.listen(2)
+print("Waiting for a connection, Server Started")
+
+
+def threaded_client(conn, player):
+    conn.send(pickle.dumps(players[player]))
+    reply = ""
+    while True:
+        try:
+            data = pickle.loads(conn.recv(2048))
+            players[player] = data
+
+            if not data:
+                print("Disconnected")
+                break
+            else:
+                if player == 1:
+                    reply = players[0]
+                else:
+                    reply = players[1]
+
+                print("Received: ", data)
+                print("Sending : ", reply)
+
+            conn.sendall(pickle.dumps(reply))
+        except:
+            break
+
+    print("Lost connection")
+    conn.close()
+
+currentPlayer = 0
+while True:
+    conn, addr = s.accept()
+    print("Connected to:", addr)
+
+    start_new_thread(threaded_client, (conn, currentPlayer))
+    currentPlayer += 1
