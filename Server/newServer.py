454bcd45dddbada756ce0ae2cd14138c9bbf7acc
Aeroking21 <55886814+Aeroking21@users.noreply.github.com>
2022-03-12 23:07:33 +0000

    checking

--- Server/newServer.py
+++ Server/newServer.py
@@ -1,17 +1,5 @@
 import socket
 import threading
-<<<<<<< HEAD
-
-#Client on the server sides that holds their attributes and waits for data
-class Client():
-    def __init__(self, name, index, conn, addr, server):
-        self.name = name
-        self.index = index
-        self.isActive = True
-        self.serverProperties = (conn, addr)
-        self.serverObj = server
-        self.dataThread = threading.Thread(target=self.listenData)
-=======
 import random
 from copy import deepcopy
 
@@ -22,7 +10,6 @@ class ClientData():
         self.serverProperties = (conn, addr)
         self.serverObj = server
         self.dataThread = threading.Thread(target=self.listenData, daemon=True)
->>>>>>> origin/main
         self.dataThread.start()
     
     #Wait for data from clients
@@ -32,15 +19,6 @@ class ClientData():
                 data = self.serverProperties[0].recv(1024).decode('utf-8')
             #If player disconnects
             except ConnectionResetError:
-<<<<<<< HEAD
-                print(f"Player {self.name} disconnected.")
-                self.isActive = False
-                self.serverObj.processServerSide("SERVERCMD: !DISCONNECT " + self.index)
-                return
-            if data:
-                print(f"Player {self.name} got data!")
-                self.processData(data)
-=======
                 self.isActive = False
                 self.serverObj.processServerSide("SERVERCMD: !DISCONNECT " + self.name)
                 print(f"Player {self.name} disconnected.")
@@ -48,25 +26,10 @@ class ClientData():
             if data:
                 self.processData(data)
                 print(f"Player {self.name} got data!")
->>>>>>> origin/main
         return
 
     #Process client side data
     def processData(self, data):
-<<<<<<< HEAD
-        if "SERVERCMD:" in data:
-            self.serverObj.processServerSide(data)
-        if data == "!DISCONNECT":
-            print(f"Player {self.name} disconnected.")
-            self.isActive = False
-            self.serverObj.processServerSide("SERVERCMD: !DISCONNECT " + str(self.name))
-        #Data processing for each client
-        self.sendClientData(data)
-        return
-
-    def sendClientData(self, data):
-        self.serverProperties[0].send(str.encode(f"Server response: {data}"))
-=======
         #Default server response
         self.sendClientData(data)
         #Server wide command
@@ -86,7 +49,6 @@ class ClientData():
     def sendClientData(self, data):
         self.serverProperties[0].send(str.encode(f"Server response: {data}"))
     
->>>>>>> origin/main
 
 
 class Server():
@@ -99,25 +61,14 @@ class Server():
         self.server.listen()
         self.listenThread = threading.Thread(target=self.addClients)
         self.listenThread.start()
-<<<<<<< HEAD
-=======
         self.turn = False #turn hasnt started till drawer is chosen
         self.next_drawer = 0 
         
->>>>>>> origin/main
         print(f"Server bind success @{self.address}")
     
     #Add new clients as they connect
     def addClients(self):
         while self.isActive:
-<<<<<<< HEAD
-            index = len(self.clientList)
-            Name = index
-            conn, addr = self.server.accept()
-            Player = Client(Name, index, conn, addr, self)
-            self.clientList.append(Player)
-            conn.send(b"Welcome to the server!\n")
-=======
             try:
                 conn, addr = self.server.accept()
                 Player = ClientData(conn, addr, self)
@@ -127,7 +78,6 @@ class Server():
             except:
                 print("Server closed.")
                 return
->>>>>>> origin/main
             
             
     #Process individual clients for data
@@ -139,10 +89,7 @@ class Server():
                 self.clientList[player].processData(data)
         return
 
-<<<<<<< HEAD
-=======
     
->>>>>>> origin/main
     #Process data on server side
     def processServerSide(self, data):
         data = data.split("SERVERCMD: ")[1]
@@ -151,37 +98,6 @@ class Server():
             self.closeServer()
         if "!DISCONNECT" in data:
             #Remove player from list based on name
-<<<<<<< HEAD
-            playerNameToRemove = data.split(" ")[1]
-            for player in self.clientList:
-                if player.name == playerNameToRemove:
-                    self.clientList.remove(playerNameToRemove)
-                    break
-            print(f"Server: Disconnected Player {playerNameToRemove}")
-
-        return
-
-    #Send data
-    def sendData(self, data, all=False, player=0):
-        if all == False:
-            self.clientList[player][0].send(str.encode(f"Server response: {data}"))
-        else:
-            for i in range(len(self.clientList)):
-                self.clientList[i][0].send(str.encode(f"Server response: {data}"))
-    
-    #Close server
-    def closeServer(self):
-        for i in range(len(self.clientList)):
-            self.clientList[i].processData("!DISCONNECT")
-        self.isActive = False
-        self.server.close()
-
-PORT = 9999
-server = Server(PORT)
-
-    
-
-=======
             playerNameToRemove = data.split("!DISCONNECT ")[1]
             for player in self.clientList:
                 if player.name.strip() == playerNameToRemove.strip():
@@ -249,4 +165,3 @@ from the client list in the server object.
 Add more functionality as you wish, and server wide commands can easily be implemented with 'processServerSide' by any client sending
 the correct command, need to do stuff like determining the current drawer and managing the XY coords etc (refer to the diagram)
 """
->>>>>>> origin/main
