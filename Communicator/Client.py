82d4488e68cdff44e02da82b83648dd5ceac7646
rana2302 <67081360+rana2302@users.noreply.github.com>
2022-03-14 17:03:18 +0000

    Update Client.py

--- Communicator/Client.py
+++ Communicator/Client.py
@@ -1,17 +1,26 @@
 import random
 import socket
 import threading
+import time
+
+PORT = 9999
+SERVER = '146.169.180.197'
 
 class Client():
-    def __init__(self, name, ip, port, gameInstance=None):
-        self.gameInstance = gameInstance
+    def __init__(self, name, ip, port):
         self.name = name            
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server.connect((ip, port))
+        self.server.settimeout(600)
+        try:
+            self.server.connect((ip, port))
+        except:
+            print("Unable to connect to server, check IP or if server is running.")
+            input()
+            exit(0)
         self.isActive = True
 
         #Begin listening for data
-        self.listenThread = threading.Thread(target=self.listenData, daemon=True)
+        self.listenThread = threading.Thread(target=self.listenData)
         self.listenThread.start()
         self.sendServer("!SETNAME " + name)
     
@@ -29,17 +38,27 @@ class Client():
             
     #Always wait for data
     def listenData(self):
+        i = 0
+        j = 0 
         while self.isActive:
+            
+            i+=1
             try:
                 #Causes issues on disconnect from other players as will be waiting for data
                 data = self.server.recv(1024).decode('utf-8')
+                j+=1
                 if len(data) > 0:
-                    self.processData(data)
-            #If connection fails
+                    self.processData(data)  #If connection fails
             except Exception as e:
                 self.isActive = False
                 print("Error in connection to server, connection lost.")
                 input()
+
+         
+            if(i+j == 1000):
+                 self.isActive = False
+                 print("you have now disconnected from the server")
+                 
         return
     
     #Process recieved data
@@ -47,8 +66,6 @@ class Client():
         print("\n" + data + '\n')
 
 if __name__ == "__main__":
-    PORT = 9999
-    SERVER = '26.168.146.5'
     Player = Client("Player-" + str(random.randint(0, 100)), SERVER, PORT)
     while True:
         data = input("Send data: ")
@@ -60,17 +77,12 @@ if __name__ == "__main__":
 """HOW IT WORKS:
 The client connects to the server and the first command it sends is a setname command,
 current randomly generated
-
 A thread begins to always listen for data from the server
-
 Data is processed by the processData method, current just printing the data to console.
-
 Sending !DISCONNECT will disconnect the current client, and will no longer have responses from the server
-
 command examples:
-
 x -> Server Response: x
 !DISCONNECT -> diconnects player
 SERVERCMD: !BROADCAST x -> sends message x to all currently connected players
-
 """
+
