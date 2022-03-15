343cd695323fe2dd96f8380f0f389f9172e92a4e
Shaanukstar123 <shaanukag0@gmail.com>
2022-03-15 23:27:06 +0000

    Textbox overflow fixed, Rescaled and Repositioned boxes

--- Main.py
+++ Main.py
@@ -30,26 +30,32 @@ class mainMenu():
         #Declare dimensions and background
         self.ip = ""
         self.ip_port = ""
-        self.width = 900
+        self.width = 950
         self.height = 550
+        self.box_width = 320
+        self.box_height =50 
+        self.box_x = 150
+        self.char_box_x = 570
+        self.char_box_size = (270,220)
         self.fpga_box = pygame.Rect(self.width-110,0,110,70)
         self.connect_box = pygame.Rect(200,413,210,50)
-        self.name_box = pygame.Rect(220,274,170,50) #Shan: this is where you need to move "Enter username" to
-        self.name_box_border = pygame.Rect(219,274,171,50)
-        self.name_box_secondary_border = pygame.Rect(170,265,277,68)
-        self.lower_box = pygame.Rect(170,345,277,140)
-        self.avatar_box = pygame.Rect(457,265,270,220)
-        self.avatar_box_white = pygame.Rect(459,267,266,216) #Shan: you need to put the avatars here and add triangles and a switching option
-        self.ip_port_box_border = pygame.Rect(224,359,160,37)
-        self.ip_port_box_bg = pygame.Rect(226,361,156,33)
+        self.name_box = pygame.Rect(self.box_x+10,274,self.box_width,50) #Shan: this is where you need to move "Enter username" to
+        self.name_box_border = pygame.Rect(self.box_x+10,274,self.box_width+1,50)
+        self.name_box_secondary_border = pygame.Rect(self.box_x,265,self.box_width+20,68)
+        self.lower_box = pygame.Rect(self.box_x,345,self.box_width+20,140)
+        self.avatar_box = pygame.Rect(self.char_box_x,265,self.char_box_size[0],self.char_box_size[1])
+        self.avatar_box_white = pygame.Rect(self.char_box_x,265,self.char_box_size[0],self.char_box_size[1]) #Shan: you need to put the avatars here and add triangles and a switching option
+        self.ip_port_box_border = pygame.Rect(self.box_x+10,359,self.box_width,37)
+        self.ip_port_box_bg = pygame.Rect(self.box_x+10,361,self.box_width,33)
         self.connect_pressed = False
         self.server_connect = False
         self.isActive = True
         self.background = pygame.image.load("Game/assets/sky_background.png")
         self.background= pygame.transform.scale(self.background,(self.width,self.height))
         self.display = pygame.display.set_mode((self.width, self.height))
-        #self.display.fill(self.white)
 
+#avatars:
+        self.avatars = []
         self.clock = pygame.time.Clock()
           
         #self.music = pygame.mixer.music.load("Game/assets/bold_statement.mp3")
@@ -58,11 +64,11 @@ class mainMenu():
         self.gifTracker = 0
         self.mouse_pos  = pygame.mouse.get_pos()
         self.username_box = GameUI.Textbox("Enter Username") #creating self.username_box object for the IP
-        self.username_box.rect.center = (305,300) #position of self.username_box on screen
+        self.username_box.rect.center = (self.box_x+100,300) #position of self.username_box on screen
         self.ip_selected = False
         self.ip_port_box = GameUI.Textbox("Enter IP: Port") #creating self.ip:port object for the IP
         self.ip_port_selected = False
-        self.ip_port_box.rect.center = (303,377) #position of self.username_box on screen
+        self.ip_port_box.rect.center = (self.box_x+100,377) #position of self.username_box on screen
 
 
         for i in range(100):
@@ -90,14 +96,15 @@ class mainMenu():
             pygame.draw.rect(self.display,(0,0,0),(self.name_box_border),2)
             pygame.draw.rect(self.display,(0,0,0),(self.name_box_secondary_border), 2)
             pygame.draw.rect(self.display,(0,0,0),(self.lower_box), 2)
-            pygame.draw.rect(self.display,(0,0,0),(self.avatar_box), 2)
-            pygame.draw.rect(self.display,(0,0,0),(self.ip_port_box_border), 2)
+            
             pygame.draw.rect(self.display,(240,227,40),(self.ip_port_box_bg))
+            pygame.draw.rect(self.display,(0,0,0),(self.ip_port_box_border), 2)
             pygame.draw.rect(self.display,(255,255,255),(self.avatar_box_white))#self.username_box for IP being drawn onto the display
-            pygame.draw.polygon(self.display,color=(200,0,200),points=[(480,376), (520,326), (520,426)])
-            pygame.draw.polygon(self.display,color=(0,0,0),points=[(478,377), (520,325), (520,428)],width=4)
-            pygame.draw.polygon(self.display,color=(200,0,200),points=[(700,376), (659,326), (659,426)])
-            pygame.draw.polygon(self.display,color=(0,0,0),points=[(702,377), (658,325), (658,428)],width=4)
+            pygame.draw.rect(self.display,(0,0,0),(self.avatar_box), 2)
+            pygame.draw.polygon(self.display,color=(200,0,200),points=[(520,376), (560,326), (560,426)])
+            pygame.draw.polygon(self.display,color=(0,0,0),points=[(520,376), (560,326), (560,426)],width=4)
+            pygame.draw.polygon(self.display,color=(200,0,200),points=[(self.char_box_x + self.char_box_size[0]+50,377), (self.char_box_x + self.char_box_size[0]+10,325), (self.char_box_x + self.char_box_size[0]+10,428)])
+            pygame.draw.polygon(self.display,color=(0,0,0),points=[(self.char_box_x + self.char_box_size[0]+50,377), (self.char_box_x + self.char_box_size[0]+10,325), (self.char_box_x + self.char_box_size[0]+10,428)],width=4)
             
             if self.fpga_connected:
                 pygame.draw.rect(self.display,(0,250,0),(self.fpga_box)) #box changes colour if not connected to fpga
@@ -137,13 +144,12 @@ class mainMenu():
         for e in pygame.event.get():
             self.display.blit(box.message,box.rect)
 
-
             if e.type == pygame.KEYDOWN:
-
                 if e.key == pygame.K_BACKSPACE:
                   box.text = box.text[:-1]
                   box.update()
-
+                elif len(box.text)>20: #text limiter for box
+                    continue
                 box.add_chr(pygame.key.name(e.key))
                 if (len(box.text) < 15):
 
